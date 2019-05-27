1a255ea50819523371476a1262952581361acdaf
Arkadi Shishlov <arkadi@agilestacks.com>
2019-05-27 16:01:56 +0300

    Terraform version requirements

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
-provider "azurerm" {
-  version = "~>1.5"
+terraform {
+  required_version = ">= 0.11.10"
+  backend "azurerm" {}
 }
 
-terraform {
-  backend "azurerm" {}
+provider "azurerm" {
+  version = "1.29.0"
+}
+
+provider "local" {
+  version = "1.2.2"
 }
